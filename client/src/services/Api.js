a53c9d3e423aba9b0819caa5b017532c7c29ee2c
Jon Roman <jon.roman@dsti.com>
2018-07-26 11:34:06 -0500

    MD-002 - Dynamically handle the Environment Settings
    The point of this commit is to allow developers to point the environment
    at the correct client port, server port and DB.

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+// NOTE baseURL: `http://localhost:8081`
+import ENV from '../../../setup/environment'
+let baseURL = 'http://10.0.3.21:' + ENV.port.server
+
 export default() => {
   return axios.create({
-    baseURL: `http://localhost:8081`
+    baseURL: baseURL
   })
 }
