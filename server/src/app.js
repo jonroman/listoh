13bc6549b5bd5b6f1515fac471b34977572071e3
Jon Roman <jon.roman@dsti.com>
2018-08-01 09:15:57 -0500

    MD-003 - Active Directory Security Added to App.js
    - Created a helper script to handle the users active directory lookup
    - Created a self invoking function to wrap app.use function to be run
    every app request.  LOCK IT DOWN!

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,33 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
 // !IMPORTANT: Routes must be defined before body parser
+// Active Directory Security MiddleWare
+
+/*
+ * @brief This is a self invoking function which handles Active Directory Security
+ * on ALL application routes. It is telling the express app to very that the AD user
+ * is valid and this function is ran before any request is made to the server.
+ * TODO: Use SESSIONS to handle the user single sign on and AD permission.
+ */
+(function getUserCredentials() {
+  User = require('../src/_activedirectory');
+  if (Object.keys(User).length === 0 && User.constructor === Object) {
+    app.use(function(req, res, next){
+      if (Object.keys(User).length === 0 && User.constructor === Object) {
+        return false;
+      } else {
+        next();
+      }
+    });
+  }
+}());
+
 // Modules
 let posts = require('./module/posts/routes');
 app.use('/posts', posts);
